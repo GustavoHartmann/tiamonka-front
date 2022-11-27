e3fb5d64b057a2d90b3192e0c705a41e3a287ce4
Gustavo <gustavolerner.hartmann@gmail.com>
2022-11-27 18:04:53 -0300

    feat: sign-in layout and functions

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,17 +4,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUpPage from "./SignUp";
 import MainPage from "./Main/Main";
 import Category from "./Category/Category";
+import SignInPage from "./SignIn";
+import AuthProvider from "../contexts/AuthContext";
 
 export default function App() {
   return (
     <Router>
       <GlobalStyleReset />
       <GlobalStyle />
-      <Routes>
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/category/:category" element={<Category />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/category/:category" element={<Category />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
---
+++ src/pages/SignIn/styles.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const SignInPageContainer = styled.div`
+  width: 100%;
+
+  margin-bottom: 50px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 200px;
+    height: 200px;
+
+    border-radius: 50%;
+
+    margin: 50px 0;
+
+    object-fit: cover;
+  }
+
+  & > div {
+    width: 700px;
+
+    background-color: white;
+
+    border: 1px solid #e5e5e5e5;
+    border-radius: 10px;
+
+    box-shadow: 0px 0px 24px -5px rgba(0, 0, 0, 0.75);
+
+    @media (max-width: 700px) {
+      width: 90%;
+    }
+  }
+`;
+
+export default SignInPageContainer;
